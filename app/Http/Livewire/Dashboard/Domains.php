77df9a401cfbe543c11c90657b3f592a24234230
Olivier Dijkstra <olivierdijkstra@gmail.com>
2020-11-18 16:27:44 +0100

    ✨  Domains can now be deleted with modal confirmation

--- app/Http/Livewire/Dashboard/Domains.php
+++ app/Http/Livewire/Dashboard/Domains.php
@@ -4,10 +4,30 @@ namespace App\Http\Livewire\Dashboard;
 
 use Illuminate\Support\Facades\Log;
 use Livewire\Component;
+use App\Models\Domain;
 
 class Domains extends Component
 {
+    public $selectedDomain = null;
+    public $deletingDomain = false;
+
     protected $listeners = ['refreshDomains' => '$refresh'];
+
+    public function openDeleteModal($id)
+    {
+        $this->deletingDomain = true;
+        $this->selectedDomain = $id;
+    }
+
+    public function deleteDomain()
+    {
+        Domain::findOrFail($this->selectedDomain)->delete();
+        
+        $this->emit('refreshDomains');
+
+        $this->selectedDomain = null;
+        $this->deletingDomain = false;
+    }
     
     public function render()
     {
--- resources/views/livewire/dashboard/domains.blade.php
+++ resources/views/livewire/dashboard/domains.blade.php
@@ -1,31 +1,62 @@
 <div>
     @foreach ($domains as $domain)
-        <div class="flex justify-between items-center m-2 px-5 py-4 rounded-md bg-gray-800">
-            <p class="text-gray-300"> {{ $domain->host }} </p>
+        <div class="flex justify-between items-center m-2 px-5 py-4 rounded-md bg-gray-700">
+            <div class="w-1/5">
+                <p class="text-gray-300"> {{ $domain->host }} </p>
+            </div>
 
-            @if ($domain->pings()->exists())
-                @if ($domain
-        ->pings()
-        ->latest()
-        ->first()->status === 200)
-                    <p class="rounded-lg px-2 py-1 bg-green-300 text-green-700">Up</p>
+            <div class="w-1/3 flex justify-between items-center">    
+                @if ($domain->pings()->exists())
+                    @if ($domain
+                    ->pings()
+                    ->latest()
+                    ->first()->status === 200)
+                        <p class="rounded-lg px-2 py-1 bg-green-300 text-green-700">Up</p>
+                    @else
+                        <p class="rounded-lg px-2 py-1  bg-red-300 text-red-600">Down</p>
+                    @endif
+
+
+                    @if ($domain->uptime() > 95)
+                        <p class="rounded-lg px-2 py-1 bg-green-300 text-green-700">{{ $domain->uptime() }}%</p>
+                    @else
+                        <p class="rounded-lg px-2 py-1 bg-red-300 text-red-700">{{ $domain->uptime() }}%</p>
+                    @endif
+
+                    <p class="text-gray-300"> {{ $domain->pings()->latest()->first()->latency }}s </p>
                 @else
-                    <p class="rounded-lg px-2 py-1  bg-red-300 text-red-600">Down</p>
+                    <p class="rounded-lg px-2 py-1 bg-blue-300 text-blue-600 text-sm">...</p>
+                    <p class="rounded-lg px-2 py-1 bg-blue-300 text-blue-600 text-sm">...</p>
+                    <p class="text-gray-300 text-sm">...</p>
                 @endif
+            </div>
 
-
-                @if ($domain->uptime() > 95)
-                    <p class="rounded-lg px-2 py-1 bg-green-300 text-green-700">{{ $domain->uptime() }}%</p>
-                @else
-                    <p class="rounded-lg px-2 py-1 bg-red-300 text-red-700">{{ $domain->uptime() }}%</p>
-                @endif
-
-                <p class="text-gray-300"> {{ $domain->pings()->latest()->first()->latency }}s </p>
-            @else
-                <p class="rounded-lg px-2 py-1 bg-blue-300 text-blue-600">Collecting data...</p>
-                <p class="rounded-lg px-2 py-1 bg-blue-300 text-blue-600">Collecting data...</p>
-                <p class="text-gray-300">Collecting data...</p>
-            @endif
+            <div class="w-1/5 flex justify-between items-center">   
+                <x-jet-danger-button wire:click="openDeleteModal({{ $domain->id }})">
+                    x
+                </x-jet-danger-button>
+            </div>
         </div>
     @endforeach
+
+
+    <x-jet-confirmation-modal wire:model="deletingDomain">
+        <x-slot name="title">
+            Delete Domain
+        </x-slot>
+    
+        <x-slot name="content">
+            Are you sure you want to delete the domain? Once your domain is deleted, all of its resources and data will be permanently deleted.
+        </x-slot>
+    
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('deletingDomain')" wire:loading.attr="disabled">
+                Nevermind
+            </x-jet-secondary-button>
+    
+            <x-jet-danger-button class="ml-2" wire:click="deleteDomain()" wire:loading.attr="disabled">
+                Delete Domain
+            </x-jet-danger-button>
+        </x-slot>
+    </x-jet-confirmation-modal>
 </div>
